ae47aac4cdc19aa0801a06a9986b396771a1364f
RyabovNA <kulek19862009@mail.ru>
2024-07-28 17:32:21 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+AmountOfHomework = 12
+NumberOfHoursSpent = 1.5
+CourseName = 'Python'
+TimePerAssignment = NumberOfHoursSpent/AmountOfHomework
+print('Курс: ', CourseName,';', 'всего задач: ', AmountOfHomework,';', 'среднее время выполнения: ', TimePerAssignment,'.')
